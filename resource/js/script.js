80c24af05cb70e34a35d9de5781b6f06ef659d72
JHeo <diner.in.erin@gmail.com>
2025-02-26 14:25:05 +0900

    commit

--- resource/js/script.js
+++ resource/js/script.js
@@ -34,10 +34,28 @@ document.addEventListener("DOMContentLoaded", function () {
 				initialView: "dayGridMonth",
 				locale: "ko",
 				selectable: true,
+				fixedWeekCount: false,
+				headerToolbar: false,
+				dayHeaderFormat: {
+					weekday: 'long'
+				},
 				dateClick: function (info) {
 					$("#selectedDate").val(info.dateStr);
 					$("#eventModal").modal("show");
-				}
+				},
+				dayCellContent: function (info) {
+					let number = document.createElement("a");
+					number.classList.add("fc-daygrid-day-number");
+					number.innerHTML = info.dayNumberText.replace("일","");
+					if (info.view.type === "dayGridMonth") {
+						return {
+							html: number.outerHTML
+						};
+					}
+					return {
+						domNodes: []
+					};
+				},
 			});
 			calendar.render();
 		}
@@ -46,3 +64,28 @@ document.addEventListener("DOMContentLoaded", function () {
 	// `#app` 내부 변화를 감지 (동적 HTML 변경 감지)
 	observer.observe(document.getElementById("app"), { childList: true, subtree: true });
 });
+
+function handleCredentialResponse(response) {
+	const data = parseJwt(response.credential);
+	document.getElementById('profile-img').src = data.picture;
+	document.getElementById('user-name').innerText = data.name;
+
+	document.getElementById('profile-icon').classList.add('hidden');
+	document.getElementById('user-info').classList.remove('hidden');
+	document.getElementById('login-button').classList.add('hidden');
+}
+
+function parseJwt(token) {
+	return JSON.parse(atob(token.split('.')[1]));
+}
+
+window.onload = function () {
+	google.accounts.id.initialize({
+		client_id: "YOUR_GOOGLE_CLIENT_ID",
+		callback: handleCredentialResponse
+	});
+	google.accounts.id.renderButton(
+		document.getElementById("login-button"),
+		{ type: "standard", theme: "outline", size: "medium" , text: "signin", width: "100", locale: "en_EN"}
+	);
+};
